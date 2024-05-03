b2f144748593f70b59c5f08b48195ac2451e89f0
grigor10 <simonyangrigor1000@mail.ru>
2024-05-03 21:26:20 +0400

    first commit

---
+++ Products.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Category {
+
+	public $id;
+	public $title;
+	public $price;
+	public $image;
+	public $description;
+	public $reviews;
+	public $category;
+	public $status;
+
+	public static function create(){
+
+	}
+
+	public static function read(){
+		
+	}
+
+	public static function update(){
+		
+	}
+
+	public static function delete(){
+		
+	}
+}
+
+
+?>
---
+++ Users.php
@@ -0,0 +1,33 @@
+<?php 
+
+class Category {
+
+	public $id;
+	public $username;
+	public $password;
+	public $firstname;
+	public $lastname;
+	public $email;
+	public $phone;
+	public $birthday;
+	public $status;
+
+	public static function create(){
+
+	}
+
+	public static function read(){
+		
+	}
+
+	public static function update(){
+		
+	}
+
+	public static function delete(){
+		
+	}
+}
+
+
+?>
